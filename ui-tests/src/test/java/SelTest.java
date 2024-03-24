3c8af3f78deb34a65775046bdc17b9d8368cdd17
maxbet233 <maks_233@list.ru>
2024-03-24 21:36:04 +0300

    add_ui_tests

--- ui-tests/src/test/java/SelTest.java
+++ ui-tests/src/test/java/SelTest.java
@@ -1,3 +1,8 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -7,18 +12,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.GeckoDriverInfo;
 
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SelTest {
 
     WebDriver driver;
     @BeforeEach
     void setup(){
-        driver = new FirefoxDriver();
-        driver.get("https://the-internet.herokuapp.com/");
+        Configuration.browser = "firefox";
+        open("https://the-internet.herokuapp.com/");
     }
 
     @Test
-    void test(){
-        WebElement inputButton = driver.findElement(By.xpath("//a[@href]='inputs'"));
-        inputButton.click();
+    void checkBoxes(){
+        SelenideElement link = $x("//a[@href='/checkboxes']");
+        link.should(visible).click();
+        ElementsCollection checkBoxes = $$x("//input[@type='checkbox']");
+        checkBoxes.get(0).click();
+        System.out.println(checkBoxes.get(0).isSelected());
+        checkBoxes.get(1).click();
+        System.out.println(checkBoxes.get(1).isSelected());
+    }
+
+    @Test
+    void dropDown(){
+        SelenideElement link = $x("//a[@href='/dropdown']");
+        link.should(visible).click();
+        SelenideElement select = $x("//select[@id='dropdown']");
+        select.click();
+        ElementsCollection options = $$x("//option[@value]");
+        options.get(1).click();
+        System.out.println(options.get(1).text());
+        select.click();
+        options.get(2).click();
+        System.out.println(options.get(2).text());
+
+    }
+
+    @AfterEach
+    void tearDown(){
+        closeWindow();
     }
 }
